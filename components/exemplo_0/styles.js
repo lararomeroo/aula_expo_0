6f796a6322483fc419b9d48b4b131fa7fb5ffc2f
lararomeroo <160646974+lararomeroo@users.noreply.github.com>
2024-02-27 15:24:09 -0300

    aula 27-02

---
+++ components/exemplo_0/styles.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'reaxt-native';
+
+function Exemplo_0 () {
+    return (
+        <View>
+            <Text> MInha primeira tela</Text>
+        </View>
+    );
+}
+
+export default Exemplo_0;
