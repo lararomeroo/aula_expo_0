92862e52243a3455950ead467c38a47077a9fcd1
lararomeroo <160646974+lararomeroo@users.noreply.github.com>
2024-03-01 09:48:23 -0300

    aula 01-03

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Exemplo_0 from './components/exemplo_0';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+     <Exemplo_0 />
       <StatusBar style="auto" />
     </View>
   );
