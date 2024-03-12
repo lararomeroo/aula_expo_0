f6f48d831fd5de8d126c64a548c8e54610dc12bf
lararomeroo <160646974+lararomeroo@users.noreply.github.com>
2024-03-12 15:23:55 -0300

    aula 12_03

---
+++ components/atividade1/index.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native';
+
+import styles from './styles';
+
+function Atividade1 () {
+    return (
+        <View style={styles.container}>
+            <Text style= {styles.texto}> Atividade 1</Text>
+            <Text style= {styles.texto}> Lara </Text>
+        </View>
+    );
+}
+
+export default Atividade1;
---
+++ components/atividade1/styles.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+ 
+const styles= StyleSheet.create({
+    container: {
+        backgroundColor: 'purple ',
+        padding: 15,
+    },
+    texto: {
+        color: '#fff',
+        fontSize: 20,
+    },
+});
+
+export default styles; 
